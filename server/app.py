007750483995c64b0478bf4024b40dac0bd9d47e
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2025-01-28 21:55:23 +0300

    correction

--- server/app.py
+++ server/app.py
@@ -14,25 +14,55 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+# Add a route for the root URL to prevent the 404 error
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to the Chatterbox API!"})
-
-@app.route('/favicon.ico')
-def favicon():
-    return '', 204
+    return "Welcome to the Flask API"
 
 @app.route('/messages')
 def messages():
-    messages = Message.query.all()
+    messages = Message.query.order_by(Message.created_at).all()
     return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET'])
 def messages_by_id(id):
     message = Message.query.get(id)
-    if not message:
-        return make_response({"error": "Message not found"}, 404)
-    return jsonify(message.to_dict())
+    if message:
+        return jsonify(message.to_dict())
+    else:
+        return jsonify({"error": "Message not found"}), 404
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if message:
+        data = request.get_json()
+        message.body = data['body']
+        db.session.commit()
+        return jsonify(message.to_dict())
+    else:
+        return jsonify({"error": "Message not found"}), 404
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({"message": "Message deleted"}), 200
+    else:
+        return jsonify({"error": "Message not found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
-from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -13,13 +13,10 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String, nullable=False)  
-    username = db.Column(db.String, nullable=False)  
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)  
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    
-    serialize_rules = ('-created_at', '-updated_at')
-
     def __repr__(self):
-        return f'<Message id={self.id}, username={self.username}, body="{self.body}">'
+        return f'<Message {self.id} {self.body}>'
